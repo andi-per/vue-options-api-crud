9b45d530d9e257bb0a1af0b720521f737a93da16
“Andi <andpermana1998@gmail.com>
2024-02-14 10:50:38 +0700

    test github access

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="container" style="margin-top: 20px">
     <div class="card">
       <div class="card-body">
-        <h5 class="card-title">SIMPLE TODO APP</h5>
+        <h5 class="card-title">TODO APP</h5>
         <div class="row">
           <div class="col-10">
             <input
